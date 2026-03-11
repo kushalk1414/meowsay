91095fb0618f7c9a75b26937074261c86dedb183
kushal karamchandani <kushal.karamchandani@nordakademie.de>
2026-03-11 16:50:49 +0100

    basic meowsay

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+// use std::env::args;
+use::colored::Colorize;
+use clap::Parser;
+
+
 fn main() {
-    println!("Hello, world!");
+    let message_struct = Message::parse();
+    message_struct.print_cat();
+}
+
+#[derive(Parser)]
+struct Message {
+    #[clap(default_value = "Meow!")]
+    message: String,
+    #[clap(short = 'd', long = "dead")]
+    dead: bool
+}
+
+
+impl Message {
+    fn print_cat(&self) {
+    
+        let eye = if self.dead {"x"} else {"o"};
+    
+        println!("{:?}", self.message);
+        println!(" \\");
+        println!("
+        /\\_/\\");
+        println!("
+        ( {eye} {eye} )", eye=eye.red().bold());
+        println!("
+        =( I )=");
+    }
+
+//     fn new(str: String) -> Message{ 
+//         Message {
+//         message: str
+//     }}
 }
